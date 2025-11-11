b083111a7f4bcc3588f52c19bb49d8d0b027a8ed
angeline-rr <ange.sofi.rr@gmail.com>
2025-11-11 11:02:01 -0300

    add typed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const typed = new Typed(".multiple-text", {
+    strings: [
+        "Eu sou uma Dev em formação...",
+        "Eu sou Musicista...",
+        "Eu sou Estudante..."
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
