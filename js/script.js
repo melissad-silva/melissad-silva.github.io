34e5355e4517eaebc19b177a9e63e0fffe783f6b
Melissa Gabriely Batista da Silva <alicefranca0204@gmail.com>
2025-11-25 09:04:18 -0300

    add theme light/dark JS

--- js/script.js
+++ js/script.js
@@ -9,3 +9,38 @@ const typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true,
 });
+
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+const themeIcon = document.querySelector('.theme-icon');
+
+function toggleTheme() {
+    body.classList.toggle('light-mode');
+    const isLightMode = body.classList.contains('light-mode');
+
+    if (isLightMode){
+        themeIcon.className = 'bx bx-moon theme-icon';
+        localStorage.setItem('theme', 'light');
+    } else {
+        themeIcon.className = 'bx bx-sun theme-icon';
+        localStorage.setItem('theme', 'dark');
+    }
+}
+
+function loadSavedTheme () {
+    const savedTheme = localStorage.setItem('theme');
+
+    if (savedTheme === 'light') {
+        body.classList.add('light-mode');
+        themeIcon.className = 'bx bx-moon theme-icon';
+    } else {
+        body.classList.remove('light-mode');
+        themeIcon.className = 'bx bx-sun theme-icon';
+    }
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', loadSavedTheme);
+} else {
+    loadSavedTheme();
+}
